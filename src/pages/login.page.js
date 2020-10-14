5cf3d4d000a2404f8ffd97bbb85b3d3568803f3a
NadHodeGW <nhode@glasswallsolutions.com>
2020-10-14 14:54:39 +0100

    login & password reset page

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -1,33 +1,22 @@
-const {
-  I
-} = inject();
+const { I } = inject();
 
 module.exports = {
-
-
   //locators
   fields: {
-    email: `input[id='Email']`,
-    password: `input[id='Password']`,
-    loginError: 'validation-error'
+    email: `input[name='email']`,
+    password: `input[name='password']`,
+    loginError: "validation-error",
   },
   links: {
-    passwordReset: `a[id='forgotPasswordLink']`,
+    passwordReset: `a[href*='pass-reminder']`,
     accountActivation: `a[id='accountActivationLink']`,
+    sowChecklist: `a[href*='sow']`,
   },
   buttons: {
     login: `button[class*='Button_button__1V1sR']`,
-    terms: `button[id='loginTermsButton']`,
-    localeDev: `button[class*='locale-dev']`,
-    localeStage: `button[class*='locale-stage']`,
-    localePerf: `button[class*='locale-perf']`,
-    localeQa: `button[class*='locale-qa1']`,
-    localePent: `button[class*='locale-pent']`,
-    closeLoginTerms: `button[id='closeLoginTerms']`,
-    modalClose: `button[class*='modal-close-button']`
-  },
-  lists: {
-    locale: `button[id='currentLocale']`
+    terms: `p[class*='Login_linkTerms__1B6HH'] > a`,
+    closeLoginTerms: "",
+    modalClose: "",
   },
 
   //Methods
@@ -38,7 +27,7 @@ module.exports = {
    */
   setEmailAddress(value) {
     const element = this.fields.email;
-    I.fillField(element, value); 
+    I.fillField(element, value);
   },
 
   /*
@@ -86,7 +75,7 @@ module.exports = {
   errorMsg() {
     let pin = I.grabTextFrom(this.fields.loginError);
   },
-  
+
   loginWith(email, password) {
     I.fillField(this.fields.email, email);
     I.fillField(this.fields.password, password);
@@ -103,42 +92,12 @@ module.exports = {
   },
 
   /*
-   * CurrentLocale
+   * SowChecklist
    * ***************************************************************
    */
-  clickCurrentLocale() {
-    const element = this.lists.locale;
+
+  clickSowChecklist() {
+    const element = this.links.sowChecklist;
     I.click(element);
   },
-
-  clickDev() {
-    const element = this.buttons.localeDev;
-    I.click(element);
-  },
-
-  clickQa1() {
-    const element = this.buttons.localeQa;
-    I.click(element);
-  },
-
-  clickStage() {
-    const element = this.buttons.localeStage;
-    I.click(element);
-  },
-
-  clickPerf() {
-    const element = this.buttons.localePerf;
-    I.click(element);
-  },
-
-  clickPent() {
-    const element = locate(this.buttons.localePent);
-    I.click(element);
-  },
-
-    
-
-  
-
-
 };
