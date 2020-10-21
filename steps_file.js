a28f43f64ba2744c401845c68a996ac132eb4b5b
fkortikov <fkortikov@thrivedetect.com>
2020-10-21 10:57:58 +0300

    added common steps for Login scenarios

--- steps_file.js
+++ steps_file.js
@@ -6,10 +6,17 @@ module.exports = function() {
   return actor({
 
     loginNoPwd: function () {
-      this.amOnPage('http://localhost:3000')
+      this.amOnPage('https://github.com/k8-proxy/p-ui-wireframes')
       loginPage.clickLogIn();
-      
     },
-    
+    onLoginPage: function() {
+      this.amOnPage('https://github.com/k8-proxy/p-ui-wireframes')
+    },
+    enterValidCredential: function () {
+      loginPage.loginWith(env.qa.email,env.qa.password)
+    },
+    enterInvalidPassword: function () {
+      loginPage.setPassword(faker.random.number())
+    }
   });
 }
