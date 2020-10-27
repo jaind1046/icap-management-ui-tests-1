780310dbaa78d55b5e0b4bcd17ba5c2c0db1079b
Anna Khvorostyamova <40318406+testprogmath@users.noreply.github.com>
2020-10-27 23:33:12 +0300

    added request-history-filtering-by-file-type steps

--- specs/features/request-history-filtering-by-file-type.feature
+++ specs/features/request-history-filtering-by-file-type.feature
@@ -7,7 +7,8 @@ Feature: request-history-filtering-by-file-type
     @TEST-163_164_165_180
     Scenario Outline: I am able to filter the risks log
         Given I have navigated to the Request History page
-        When I click on the Add Filter button and add a filter selection as <filter>
+        When I click on the More Filters button
+        And I click on the Add Filter button and add a filter selection as <filter>
         Then the result list only shows filtered files as <filteredFile>
         Examples:
             | filter        | filteredFile |
--- src/pages/request-history.page.js
+++ src/pages/request-history.page.js
@@ -1,87 +1,87 @@
 const MyHelper = require("../utils/helper");
 var moment = require('moment');
 const {
-  I
+    I
 } = inject();
 
 module.exports = {
     //Locators
 
-  fields: {
-    inputFilterFileID: `div[id='inputFilterTransactionID'] > input`,
-    customPaginatorGoTo: `input[class*='custom-paginator-goto']`,
-    datetimeFrom: 'datetime-local-left',
-    //`input[id='datetime-local-left']`,
-    datetimeTo: `input[id='datetime-local-right']`,
-    from: `div[class='jss3']`,
-  },
-  options: {
-    countOfFiles: ""
-  },
-  buttons: {
-    moreFilters: `button[class*='Filters_moreFilters__']`,
-    dateTime: `//button[contains(.,'Date/Time')]`,
-    time_1hour: 'button:nth-child(1) > p',
-    time_12hours: 'button:nth-child(2) > p',
-    time_24hours: 'button:nth-child(3) > p',
-    addFilter: `button[class*='Button_button__1V1sR']`,
-    deleteAppliedFilter: `button[class^='SelectedFilter_buttonClose__']`,
-    fileTypeMenu: "",
-    fileOutcomeMenu: "",
-    fileOutcomeFilterSafe: "",
-    fileOutcomeFilterDangerous: "",
-    fileOutcomeFilterBlocked: "",
-    fileOutcomeFilterChecked: "",
-    fileOutcomeFilterUnclassified: "",
-    fileTypeMenuAdd: "",
-    fileIdAdd: "",
-    gotoPage: "",
-    previousPage: "",
-    firstPage: "",
-    nextPage: "",
-    lastPage: "",
-  },
-  table: {
-    historyTable: `div[class*='RequestHistory_wrapTable__13V_o']`,
-    fileTableBody: `tbody[class*='MuiTableBody-root']`,
-  },
-  checkboxes: {
-    fileTypeDoc: "",
-    fileTypeDot: "",
-    fileTypeDocx: "",
-    fileTypeDocm: "",
-    fileTypeXlsx: "",
-    fileTypeXls: "",
-    fileTypeXlsm: "",
-    fileTypePpt: "",
-    fileTypePptx: "",
-    fileTypeJpeg: "",
-    fileTypePng: "",
-    fileTypeGif: "",
-    fileTypePdf: "",
-  },
-  calendar: {
-    dateTimePicker: "",
-  },
-  popup: {
-    filterFileId: `div[id='filterMenuPopup'] > ul > li:nth-of-type(2) > button`,
-    filterType: `div[id='filterMenuPopup'] > ul > li:nth-of-type(5) > button`,
-    filterFileOutcomes: `div[id='filterMenuPopup'] > ul > li:nth-of-type(3) > button`,
-    filterMenu: `div[id='filterMenuPopup']`,
-  },
-  containers: {
-    appliedFilterFamily: `div[class^=SelectedFilter_SelectedFilter__]`,
-    appliedFilters: `div[class*=Filters_filters__] > div`
-  },
-  modal: {
-    modalHeader: `section[class*='FileInfo_FileInfo__1Z457'] > header`,
-    cmpDetailsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(5) > div > label`,
-    issueItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(2)`,
-    sanitisationItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(3)`,
-    remedyItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(4)`,
-  },
+    fields: {
+        inputFilterFileID: `div[id='inputFilterTransactionID'] > input`,
+        customPaginatorGoTo: `input[class*='custom-paginator-goto']`,
+        datetimeFrom: 'datetime-local-left',
+        //`input[id='datetime-local-left']`,
+        datetimeTo: `input[id='datetime-local-right']`,
+        from: `div[class='jss3']`,
+    },
+    options: {
+        countOfFiles: ""
+    },
+    buttons: {
+        moreFilters: `button[class*='Filters_moreFilters__']`,
+        dateTime: `//button[contains(.,'Date/Time')]`,
+        time_1hour: 'button:nth-child(1) > p',
+        time_12hours: 'button:nth-child(2) > p',
+        time_24hours: 'button:nth-child(3) > p',
+        addFilter: `button[class*='Button_button__1V1sR']`,
+        deleteAppliedFilter: `button[class^='SelectedFilter_buttonClose__']`,
+        fileTypeMenu: "",
+        fileOutcomeMenu: "",
+        fileOutcomeFilterSafe: "",
+        fileOutcomeFilterDangerous: "",
+        fileOutcomeFilterBlocked: "",
+        fileOutcomeFilterChecked: "",
+        fileOutcomeFilterUnclassified: "",
+        fileTypeMenuAdd: "",
+        fileIdAdd: "",
+        gotoPage: "",
+        previousPage: "",
+        firstPage: "",
+        nextPage: "",
+        lastPage: "",
+    },
+    table: {
+        historyTable: `div[class*='RequestHistory_wrapTable__13V_o']`,
+        fileTableBody: `tbody[class*='MuiTableBody-root']`,
+    },
+    checkboxes: {
+        fileTypeDoc: "",
+        fileTypeDot: "",
+        fileTypeDocx: "",
+        fileTypeDocm: "",
+        fileTypeXlsx: "",
+        fileTypeXls: "",
+        fileTypeXlsm: "",
+        fileTypePpt: "",
+        fileTypePptx: "",
+        fileTypeJpeg: "",
+        fileTypePng: "",
+        fileTypeGif: "",
+        fileTypePdf: "",
+    },
+    calendar: {
+        dateTimePicker: "",
+    },
+    popup: {
+        filterFileId: `div[id='filterMenuPopup'] > ul > li:nth-of-type(2) > button`,
+        filterType: `div[id='filterMenuPopup'] > ul > li:nth-of-type(5) > button`,
+        filterFileOutcomes: `div[id='filterMenuPopup'] > ul > li:nth-of-type(3) > button`,
+        filterMenu: `div[id='filterMenuPopup']`,
+    },
+    containers: {
+        appliedFilterFamily: `div[class^=SelectedFilter_SelectedFilter__]`,
+        appliedFilters: `div[class*=Filters_filters__] > div`
+    },
+    modal: {
+        modalHeader: `section[class*='FileInfo_FileInfo__1Z457'] > header`,
+        cmpDetailsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(5) > div > label`,
+        issueItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(2)`,
+        sanitisationItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(3)`,
+        remedyItemsBanner: `div[class*='FileInfo_inner__1NnWT'] > div:nth-of-type(4)`,
+    },
 
-  //Methods
+    //Methods
 
     /*
      * Datetimepicker
@@ -92,79 +92,79 @@ module.exports = {
         return await I.grabAttributeFrom(element, jsonValue());
     },
 
-  setDatetimepicker(value) {
-    const element = this.calendar.dateTimePicker;
-    I.fillField(element, value);
-  },
+    setDatetimepicker(value) {
+        const element = this.calendar.dateTimePicker;
+        I.fillField(element, value);
+    },
 
-  openTimeMenu() {
-    const element = this.buttons.dateTime;
-    I.click(element);
-  },
+    openTimeMenu() {
+        const element = this.buttons.dateTime;
+        I.click(element);
+    },
 
-  selectTimePeriod(period) {
-    if (period == '1 Hour') {
-      I.click(this.buttons.time_1hour)
-    } else if (period == '12 Hours') {
-      I.click(this.buttons.time_12hours)
-    } else if (period == '24 Hours') {
-      I.click(this.buttons.time_24hours)
-    }
+    selectTimePeriod(period) {
+        if (period == '1 Hour') {
+            I.click(this.buttons.time_1hour)
+        } else if (period == '12 Hours') {
+            I.click(this.buttons.time_12hours)
+        } else if (period == '24 Hours') {
+            I.click(this.buttons.time_24hours)
+        }
 
-  },
+    },
 
-  getTimeFrom() {
-    const element = this.fields.datetimeFrom;
-    I.grabValueFrom(element);
+    getTimeFrom() {
+        const element = this.fields.datetimeFrom;
+        I.grabValueFrom(element);
 
-  },
+    },
 
-  async getTimeTo() {
-    const element = this.fields.datetimeTo;
-    let timeTo = await I.grabTextFrom(this.fields.datetimeTo);
-    return timeTo;
-  },
+    async getTimeTo() {
+        const element = this.fields.datetimeTo;
+        let timeTo = await I.grabTextFrom(this.fields.datetimeTo);
+        return timeTo;
+    },
 
-  setTimeFrom(dateFrom) {
-    const element = this.fields.datetimeFrom;
-    I.fillField(element, dateFrom);
-  },
+    setTimeFrom(dateFrom) {
+        const element = this.fields.datetimeFrom;
+        I.fillField(element, dateFrom);
+    },
 
-  setTimeTo(dateTo) {
-    const element = this.fields.datetimeTo;
-    I.fillField(element, dateTo);
-  },
+    setTimeTo(dateTo) {
+        const element = this.fields.datetimeTo;
+        I.fillField(element, dateTo);
+    },
 
-  assertAccurateTimeFromIsSet(datetimeFrom) {
-    let x = document.getElementById(this.fields.datetimeFrom).ATTRIBUTE_NODE(value);
-    if (datetimeFrom == '1 hour earlier') {
-      I.seeTextEquals(this.getPreviousHour(), x)
-    } else if (period == '12 hours earlier') {
-      I.see(MyHelper.getPrevious12Hour(), this.fields.datetimeFrom)
-    } else if (period == '24 hours earlier') {
-      I.see(MyHelper.getPrevious24Hour(), this.fields.datetimeFrom)
-    }
-  },
+    assertAccurateTimeFromIsSet(datetimeFrom) {
+        let x = document.getElementById(this.fields.datetimeFrom).ATTRIBUTE_NODE(value);
+        if (datetimeFrom == '1 hour earlier') {
+            I.seeTextEquals(this.getPreviousHour(), x)
+        } else if (period == '12 hours earlier') {
+            I.see(MyHelper.getPrevious12Hour(), this.fields.datetimeFrom)
+        } else if (period == '24 hours earlier') {
+            I.see(MyHelper.getPrevious24Hour(), this.fields.datetimeFrom)
+        }
+    },
 
-  checkAccurateTime() {
-    let timeFrom = document
-    getElementById(this.fields.datetimeFrom).ATTRIBUTE_NODE(value)
-    I.seeTextEquals(this.getPreviousHour(), timeFrom)
-  },
+    checkAccurateTime() {
+        let timeFrom = document
+        getElementById(this.fields.datetimeFrom).ATTRIBUTE_NODE(value)
+        I.seeTextEquals(this.getPreviousHour(), timeFrom)
+    },
 
-  getPreviousHour() {
-    var now = moment().utc(true);
-    var previousHour = now.subtract(1, 'h').format(String).slice(0, -1);
-    return previousHour;
-  },
+    getPreviousHour() {
+        var now = moment().utc(true);
+        var previousHour = now.subtract(1, 'h').format(String).slice(0, -1);
+        return previousHour;
+    },
 
-  assertAccurateTimeToIsSet(datetimeTo) {
-    var currentTime = moment().utc(true).format().slice(0, -1);
-    let x = document.getElementById(this.fields.datetimeTo).ATTRIBUTE_NODE(value);
-    if (datetimeTo == 'current time') {
-      I.seeTextEquals(currentTime, x);
-    }
-  },
+    assertAccurateTimeToIsSet(datetimeTo) {
+        var currentTime = moment().utc(true).format().slice(0, -1);
+        let x = document.getElementById(this.fields.datetimeTo).ATTRIBUTE_NODE(value);
+        if (datetimeTo == 'current time') {
+            I.seeTextEquals(currentTime, x);
+        }
+    },
 
 
     /*
@@ -173,6 +173,10 @@ module.exports = {
      */
     clickAddFilterButton() {
         const element = this.buttons.addFilter;
+        I.click(element);
+    },
+    clickMoreFiltersButton() {
+        const element = this.buttons.moreFilters;
         I.click(element);
     },
 
@@ -194,15 +198,15 @@ module.exports = {
         I.click(outcomeType);
     },
 
-  clickFileTypeAdd() {
-    const element = this.buttons.fileTypeMenuAdd;
-    I.click(element);
-  },
+    clickFileTypeAdd() {
+        const element = this.buttons.fileTypeMenuAdd;
+        I.click(element);
+    },
 
-  setFileId(value) {
-    const element = this.fields.inputFilterFileID;
-    I.fillField(element, value);
-  },
+    setFileId(value) {
+        const element = this.fields.inputFilterFileID;
+        I.fillField(element, value);
+    },
 
     setFileType(value) {
         const checkboxLabel = value.toUpperCase();
@@ -257,12 +261,11 @@ module.exports = {
         listOfFilters.forEach(e => {
             const filterFooter = e.lastElementChild.children.item(0);
             if (filterFooter.textContent === filterValue) {
-                const deleteButton = document.getElementsByClassName
-                (
+                const deleteButton = document.getElementsByClassName(
                     e.firstElementChild
-                        .children
-                        .item(1)
-                        .className
+                    .children
+                    .item(1)
+                    .className
                 );
                 I.click(`button[class*='` + deleteButton + `']`);
             }
@@ -275,6 +278,12 @@ module.exports = {
             I.seeInField(row + '> td:nth-child(2)', filteredFile);
         }
     },
+    checkFileTypeValues(filteredFile) {
+        const table = document.getElementsByTagName('table')
+        for (let row in table.tBodies[0].rows) {
+            I.seeInField(row + '> td:nth-child(3)', filteredFile);
+        }
+    },
 
     /*
      * Pagination
@@ -285,39 +294,39 @@ module.exports = {
         I.click(element);
     },
 
-  clickPrevious() {
-    const element = this.buttons.previousPage;
-    I.click(element);
-  },
+    clickPrevious() {
+        const element = this.buttons.previousPage;
+        I.click(element);
+    },
 
-  clickLast() {
-    const element = this.buttons.lastPage;
-    I.click(element);
-  },
+    clickLast() {
+        const element = this.buttons.lastPage;
+        I.click(element);
+    },
 
-  clickPrevious() {
-    const element = this.buttons.nextPage;
-    I.click(element);
-  },
+    clickPrevious() {
+        const element = this.buttons.nextPage;
+        I.click(element);
+    },
 
-  setCustomPage(value) {
-    const element = this.fields.customPaginatorGoTo;
-    I.fillField(element, value);
-  },
+    setCustomPage(value) {
+        const element = this.fields.customPaginatorGoTo;
+        I.fillField(element, value);
+    },
 
-  clickGo() {
-    const element = this.buttons.go;
-    I.click(element);
-  },
+    clickGo() {
+        const element = this.buttons.go;
+        I.click(element);
+    },
 
-  /*
-   * Opening file details
-   * ***************************************************************
-   */
+    /*
+     * Opening file details
+     * ***************************************************************
+     */
 
-  getFileRecord(fileId) {
-    return "//tr[contains(., '" + fileId + "')]"
-  },
+    getFileRecord(fileId) {
+        return "//tr[contains(., '" + fileId + "')]"
+    },
 
     openFileRecord(fileId) {
         I.click(this.getFileRecord(fileId))
